a70a694a256c1178a6c8244e44b70bfa1ffe0ca3
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2017-09-18 20:51:36 +0900

    Work correctly

--- AtmosphericPressure/ViewController.swift
+++ AtmosphericPressure/ViewController.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    let altimeter = CMAltimeter()
+    
+    @IBOutlet weak var pressureLabel: UILabel!
+
+    @IBOutlet weak var altitudeLabel: UILabel!
+    
+    @IBAction func doReset(_ sender: Any) {
+        altimeter.stopRelativeAltitudeUpdates()
+        startUpdate()
+    }
+
+    func startUpdate() {
+        self.pressureLabel.text = "気圧:---- hPa"
+        self.altitudeLabel.text = "高さ:-,-- m"
+        if (CMAltimeter.isRelativeAltitudeAvailable()) {
+            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: {
+                data, error in
+                if error == nil {
+                    let pressure:Double = Double(data!.pressure)
+                    let altitude:Double = Double(data!.relativeAltitude)
+                    self.pressureLabel.text = String(format: "気圧:%.1f hPa", pressure * 10)
+                    self.altitudeLabel.text = String(format: "高さ:%.2f m", altitude)
+                }
+            })
+        }
+        else {
+            print("not use altimeter")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
